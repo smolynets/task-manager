14e7be8c0b8f10f6a1b45107f30bcfaef5ff23b5
Oleh <smolynets@gmail.com>
2018-06-08 03:55:13 +0300

    add one_task page

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.list, name='list'),
+    url(r'^task/(?P<pk>\d+)/', views.one_task, 
+    	name='one_task'),
 ]
--- taskmanager/views.py
+++ taskmanager/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 from .models import *
 
+
+
 def list(request):
 	tasks = Task.objects.all()
 	return render(request, 'taskmanager/home.html', {'task': tasks})
+
+
+
+def one_task(request, pk):
+  task = Task.objects.filter(pk=pk)
+  return render(request, 'taskmanager/one_task.html', {'task':task})
